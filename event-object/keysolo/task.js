cfa47ac74144a3088137b067fc77d69c501c3363
Mariya <marixasha@mail.ru>
2026-01-22 23:37:31 +0300

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,20 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keyup', (event) => {
+    const expectedChar = this.currentSymbol.textContent.trim().toLowerCase();
+    const inputChar = event.key.toLowerCase();
+    
+    if (!/^[a-za-я]$/.test(inputChar)) {
+      return; 
+    }
+
+    if (inputChar === expectedChar) {
+      this.success();
+    } else {
+      this.fail();
+    }
+  });
   }
 
   success() {
